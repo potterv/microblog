b9d3a160aa564f760d923f77ba056205256ab311
potter <gonchar.vladimir@hotmail.com>
2018-07-30 06:43:57 +0300

    Шаблоны и flask

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ from flask import render_template
 from app import app
 
 @app.route('/')
-#@app.route('/index')
+@app.route('/index')
 def index():
-    return "Hello, World!"
+    user={'username':'vlad'}
+    return render_template('index.html', title='Home', user=user)
