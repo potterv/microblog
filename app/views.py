21e8aa71baf0d6843ea43d270927258a5a489b7a
potterv <gonchar.vladimir@hotmail.com>
2018-07-31 09:41:24 +0300

    Условные и циклические конструкции в шаблонах

--- app/views.py
+++ app/views.py
@@ -2,15 +2,19 @@ from flask import render_template
 from app import app
 @app.route('/index')
 def index():
-    user = { 'nickname': 'Miguel' } # выдуманный пользователь
-    posts = [ # список выдуманных постов
+    user = {'username': 'Эльдар Рязанов'}
+    posts = [
         {
-            'author': { 'nickname': 'John' },
+            'author': {'username': 'John'},
             'body': 'Beautiful day in Portland!'
         },
         {
-            'author': { 'nickname': 'Susan' },
+            'author': {'username': 'Susan'},
             'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Ипполит'},
+            'body': 'Какая гадость эта ваша заливная рыба!!'
         }
     ]
-    return render_template("index.html",title = 'Home', user = user,  posts = posts)
+    return render_template('index.html', title='Home', user=user, posts=posts)
