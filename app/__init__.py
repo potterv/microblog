63eaeeff4d18fe68569ce1e12328f662f06600eb
potterv <gonchar.vladimir@hotmail.com>
2018-08-02 16:19:35 +0300

    It works with base "start".

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from config import Config
 app = Flask(__name__)
 app.config.from_object(Config)
-
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 #from app import routes
-from app import views
+from app import views, models
